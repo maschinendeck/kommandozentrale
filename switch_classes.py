f2b661b24cc4e1b26a1ca945c0ef668b9537ff85
pajowu <github@ca.pajowu.de>
2016-03-08 17:24:41 +0200

    add switchclass, which provides some useful defaults for new switch classes

--- switch_classes.py
+++ switch_classes.py
@@ -1,12 +1,27 @@
 import shelve
-class OnOffSwitch():
-	state = False
-	methods = ["on", "off"]
+class SwitchClass():
+	state = None
+	methods = []
 	def __init__(self, initial_data={}, **kwargs):
 		initial_data.update(kwargs)
 		for k,v in initial_data.items():
 			setattr(self, k, v)
 		self.state = self.getCurrentState()
+
+	def getCurrentState(self):
+		# get state from cache
+		with shelve.open('kommandozentrale.db') as db:
+			if self.name in db:
+				return db[self.name]
+			else:
+				db[self.name] = False
+
+	def getState(self):
+		return self.state
+
+
+class OnOffSwitch(SwitchClass):
+	methods = ["on", "off"]
 
 	def on(self):
 		self.state = True
@@ -16,13 +31,5 @@ class OnOffSwitch():
 		self.state = False
 		return self.state
 
-	def getState(self):
-		return self.state
 
-	def getCurrentState(self):
-		# get state from cache
-		with shelve.open('kommandozentrale.db') as db:
-			if self.name in db:
-				return db[self.name]
-			else:
-				db[self.name] = False
+
